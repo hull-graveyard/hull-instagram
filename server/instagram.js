a2d17aeb7bd73358b984947f1cbd1ba3b35978d7
Romain Dardour <romain@hull.io>
2016-02-29 14:06:48 +0100

    revert to restler

--- server/instagram.js
+++ server/instagram.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import logError from './lib/log-error';
 import cache from './lib/hull-cache';
-import axios from 'axios';
+import restler from 'restler';
 import moment from 'moment';
 import URI from 'urijs';
 
@@ -30,16 +30,17 @@ export default {
         type: 'services/instagram_app'
       }) || {});
       function createSubscription() {
-        const payload = {
-          object: 'user',
-          aspect: 'media',
-          client_id: key,
-          client_secret: instaSecret,
-          verify_token: SHIP_TOKEN,
-          callback_url: cbu
-        };
-        return axios.post(INSTAGRAM_SUBSCRIPTION_URL, payload)
-        .then((response) => {
+        return restler.post(INSTAGRAM_SUBSCRIPTION_URL, {
+          data: {
+            callback_url: cbu,
+            aspect: 'media',
+            object: 'user',
+            client_id: key,
+            client_secret: instaSecret,
+            verify_token: SHIP_TOKEN
+          }
+        })
+        .on('success', (response) => {
           // If successful, store the subscription id in the ship
           // Ensure we don't trigger a ship update that would triger a re-register
           if (response && response.meta && response.meta.code === 200) {
@@ -47,11 +48,13 @@ export default {
             });
           }
           return response;
-        });
+        })
+        .on('fail', logError)
+        .on('error', logError);
       }
 
       function deleteSubscription(subscriptionId) {
-        return axios.delete(INSTAGRAM_SUBSCRIPTION_URL, {
+        return restler.del(INSTAGRAM_SUBSCRIPTION_URL, {
           client_id: key,
           client_secret: instaSecret,
           id: subscriptionId
@@ -62,9 +65,11 @@ export default {
       // Delete the previous subscription if we have one.
       if (subscriptionId) {
         console.log('Deleting subscription', subscriptionId);
-        return deleteSubscription().then(createSubscription, createSubscription);
+        return deleteSubscription()
+        .on('complete', createSubscription)
+        .on('fail', logError)
+        .on('error', logError);
       }
-
       // No previous subscription stored, just create it.
       return createSubscription();
     }, logError)
@@ -83,72 +88,69 @@ export default {
         // Get Settings to retreive instagram credentials - cached
         cache(user, 'app/settings')
 
-        .then(({ auth: { instagram: { access_token: token } } }) =>
+        .then(({ auth: { instagram: { access_token: token } } }) => {
           // With the instagram token, fetch the newly created media
-          axios.get(`${INSTAGRAM_MEDIA_URL}/${mid}?access_token=${token}`)
-        , logError)
+          restler.get(`${INSTAGRAM_MEDIA_URL}/${mid}?access_token=${token}`)
+          .on('success', (media = {}) => {
+            const { status, data: { data } } = media;
 
-        .then((media = {}) => {
+            // Todo: we could update the user's instagram data from here.
+            // console.log(data.user);
+            // { username: 'unity',
+            // profile_picture: 'https://scontent.cdninstagram.com/t51.2885-19/s150x150/1169138_1684969021751603_1313711738_a.jpg',
+            // id: '361536',
+            // full_name: 'Romain Dardour' }
 
-          const { status, data: { data } } = media;
-
-          // Todo: we could update the user's instagram data from here.
-          // console.log(data.user);
-          // { username: 'unity',
-          // profile_picture: 'https://scontent.cdninstagram.com/t51.2885-19/s150x150/1169138_1684969021751603_1313711738_a.jpg',
-          // id: '361536',
-          // full_name: 'Romain Dardour' }
-
-          // Dig into the media to return the right info.
-          if (!data || status !== 200) {
-            throw new Error(`No Image found: status:${status} mid:${mid}`);
-          }
-          // UNIX timestamp
-          const createdAt = moment(data.created_time, 'X').format();
-          const pictureLink = new URI(data.images.standard_resolution.url).removeSearch(/.*/).toString();
-          const description = data.caption.text;
-
-          const context = {
-            created_at: createdAt,
-            source: 'instagram',
-            ip: null,
-            url: null,
-            referer: null
-          };
-
-          const track = {
-            description,
-            picture: pictureLink,
-            link: data.link,
-            filter: data.filter,
-            tags: data.tags.join(', '),
-            location: data.location,
-            instagram_id: data.id
-          };
-
-          const image = {
-            name: description,
-            tags: data.tags,
-            source_url: pictureLink,
-            description: data.caption.text,
-            created_at: createdAt,
-            extra: {
-              link: data.link
+            // Dig into the media to return the right info.
+            if (!data || status !== 200) {
+              throw new Error(`No Image found: status:${status} mid:${mid}`);
             }
-          };
+            // UNIX timestamp
+            const createdAt = moment(data.created_time, 'X').format();
+            const pictureLink = new URI(data.images.standard_resolution.url).removeSearch(/.*/).toString();
+            const description = data.caption.text;
 
-        
-          // user.post('/app/activity', {
-          //   verb: 'post',
-          //   title: description,
-          //   content: description,
-          //   picture: pictureLink,
-          //   extra: { tags: data.tags }
-          //   // uid
-          // });
-          user.post('/me/images', image).catch(logError);
-          // Do a Hull track as the user.
-          return user.track(`New instagram ${data.type}`, track, context);
+            const context = {
+              created_at: createdAt,
+              source: 'instagram',
+              ip: null,
+              url: null,
+              referer: null
+            };
+
+            const track = {
+              description,
+              picture: pictureLink,
+              link: data.link,
+              filter: data.filter,
+              tags: data.tags.join(', '),
+              location: data.location,
+              instagram_id: data.id
+            };
+
+            const image = {
+              name: description,
+              tags: data.tags,
+              source_url: pictureLink,
+              description: data.caption.text,
+              created_at: createdAt,
+              extra: {
+                link: data.link
+              }
+            };
+
+            // user.post('/app/activity', {
+            //   verb: 'post',
+            //   title: description,
+            //   content: description,
+            //   picture: pictureLink,
+            //   extra: { tags: data.tags }
+            //   // uid
+            // });
+            user.post('/me/images', image).catch(logError);
+            // Do a Hull track as the user.
+            return user.track(`New instagram ${data.type}`, track, context);
+          });
         }, logError)
 
         .catch(logError);
